df27dffb1ed403ad91b8d0d9c52110f0bb04149e
Jay <jgoel@jays-air.mynetworksettings.com>
2024-06-27 07:45:52 -0400

    enqueue messages

--- queue/pebble/pebble.go
+++ queue/pebble/pebble.go
@@ -1,9 +1,12 @@
 package pebble
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"log"
 	"q/models"
+	"slices"
 	"sync"
 	"time"
 
@@ -12,15 +15,15 @@ import (
 	"github.com/cockroachdb/pebble"
 )
 
-type Message struct {
-	id      *snowflake.ID
-	message *string
-}
+// type Message struct {
+// 	id      *snowflake.ID
+// 	message *string
+// }
 
 type PebbleQueue struct {
 	filename string
 	db       *pebble.DB
-	ch       chan Message
+	// ch       chan Message
 	mu       *sync.Mutex
 	snow     *snowflake.Node
 	ticker   *time.Ticker
@@ -42,6 +45,88 @@ type PebbleQueue struct {
 // 	},
 // 	[]string{"tenant_id", "queue", "status"},
 // )
+
+type KVType byte
+type MessageStatus byte
+
+const (
+	KV_QUEUE    KVType = 1
+	KV_MESSAGE  KVType = 2
+	KV_METADATA KVType = 3
+	KV_KEYVALUE KVType = 4
+
+	MAX_QUEUE_NAME_LENGTH int = 80
+
+	MessageQueued MessageStatus = 1
+	MessageDequed MessageStatus = 2
+
+	MaxReadyQueueMessages uint64 = 50_000
+)
+
+func QueueToKey(tenantID int64, queue string) []byte {
+	// type + tenantid + queue
+	rc := make([]byte, 1+8+80)
+
+	rc[0] = byte(KV_QUEUE)
+	binary.BigEndian.PutUint64(rc[1:], uint64(tenantID))
+	copy(rc[8:], queue)
+
+	return rc
+}
+
+func KeyToQueue(key []byte) (tenantId int64, queue string) {
+	tenantUint := binary.BigEndian.Uint64(key[1:9])
+	tenantId = int64(tenantUint)
+
+	n := bytes.IndexByte(key[8:], 0)
+	queue = string(key[8 : 8+n])
+
+	return
+}
+
+func MessageToKey(tenantID int64, queue string, messageId int64) []byte {
+	// type + tenantid + queue + messageid
+	rc := QueueToKey(tenantID, queue)
+
+	rc[0] = byte(KV_MESSAGE)
+	binary.BigEndian.AppendUint64(rc, uint64(messageId))
+
+	return rc
+}
+
+func QueuedMetadataToKey(tenantID int64, queue string, messageId int64, deliverAt int64) []byte {
+	// type + tenantid + queue + status + deliverat + messageid
+	rc := QueueToKey(tenantID, queue)
+
+	rc[0] = byte(KV_METADATA)
+	rc = append(rc, byte(MessageQueued))
+	binary.BigEndian.AppendUint64(rc, uint64(deliverAt))
+	binary.BigEndian.AppendUint64(rc, uint64(messageId))
+
+	return rc
+}
+
+func DequeuedMetadataToKey(tenantID int64, queue string, messageId int64, redeliverAt int64) []byte {
+	// type + tenantid + queue + status + redeliverat + messageid
+	rc := QueueToKey(tenantID, queue)
+
+	rc[0] = byte(KV_METADATA)
+	rc = append(rc, byte(MessageDequed))
+	binary.BigEndian.AppendUint64(rc, uint64(redeliverAt))
+	binary.BigEndian.AppendUint64(rc, uint64(messageId))
+
+	return rc
+}
+
+func KVToKey(tenantID int64, queue string, messageId int64, k string) []byte {
+	// type + tenantid + queue + messageid + k
+	rc := MessageToKey(tenantID, queue, messageId)
+
+	rc[0] = byte(KV_KEYVALUE)
+	slices.Concat(rc, []byte(k))
+
+	return rc
+}
 
 func NewPebbleQueue() *PebbleQueue {
 	snow, err := snowflake.NewNode(1)
@@ -68,35 +153,17 @@ func NewPebbleQueue() *PebbleQueue {
 	// rc.queued.Add(1)
 
 	go func() {
-		var maxMessages uint64 = 100_000
 
 		for {
 			select {
 			case <-rc.ticker.C:
-
-				i, err := db.NewIter(
-					&pebble.IterOptions{
-						// UpperBound: []byte{1 << 7, 0, 0, 0, 0, 0, 0, 0},
-					},
-				)
-				if err != nil {
-					log.Println(err)
-				}
-
-				log.Println(i.First(), i.Valid())
-
-				for i.First(); i.Valid(); i.Next() {
-					log.Println(i.Key())
-					break
-				}
-
 				rc.mu.Lock()
-				log.Println("queued size", rc.queued.GetSizeInBytes(), rc.queued.GetCardinality())
-				log.Println("dequeued size", rc.dequeued.GetSizeInBytes(), rc.dequeued.GetCardinality())
+				// log.Println("queued size", rc.queued.GetSizeInBytes(), rc.queued.GetCardinality())
+				// log.Println("dequeued size", rc.dequeued.GetSizeInBytes(), rc.dequeued.GetCardinality())
 
 				// log.Println(rc.queued.Maximum())
 				// should it be 0? instead, if it is less than maxcapacity
-				if rc.queued.GetCardinality() < maxMessages {
+				if rc.queued.GetCardinality() < MaxReadyQueueMessages {
 					// 	// get max from bitmap
 					var maxQueued uint64 = 0
 					var maxDequeued uint64 = 0
@@ -128,7 +195,7 @@ func NewPebbleQueue() *PebbleQueue {
 						id := binary.BigEndian.Uint64(iter.Key())
 						rc.queued.Add(id)
 
-						if rc.queued.GetCardinality() >= maxMessages {
+						if rc.queued.GetCardinality() >= MaxReadyQueueMessages {
 							break
 						}
 					}
@@ -186,38 +253,90 @@ func NewPebbleQueue() *PebbleQueue {
 }
 
 func (q *PebbleQueue) CreateQueue(tenantId int64, queue string) error {
-	return nil
+	if len(queue) == 0 {
+		return errors.New("invalid queue name")
+	}
+
+	key := QueueToKey(tenantId, queue)
+	err := q.db.Set(key, nil, pebble.Sync)
+	return err
 }
 
 func (q *PebbleQueue) DeleteQueue(tenantId int64, queue string) error {
+	// key: q (1) + tenantId (8) + queueName (80) = 1
+	// key: msgs (1) + tenantId (8) + queueName (80) + status (1) + msgid (8) = message
+	// xxx key: msgs (1) + tenantId (8) + msgid (8) = message
 	return nil
 }
 
 func (q *PebbleQueue) ListQueues(tenantId int64) ([]string, error) {
-	return nil, nil
+	lower := QueueToKey(tenantId, "")
+
+	upper := QueueToKey(tenantId, "")
+	upper[0] += 1
+
+	iter, err := q.db.NewIter(&pebble.IterOptions{
+		LowerBound: lower,
+		UpperBound: upper,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	rc := make([]string, 0)
+
+	for iter.First(); iter.Valid(); iter.Next() {
+		_, queue := KeyToQueue(iter.Key())
+		rc = append(rc, queue)
+	}
+
+	iter.Close()
+
+	return rc, nil
 }
 
 func (q *PebbleQueue) Enqueue(tenantId int64, queue string, message string, kv map[string]string, delay int, requeueIn int) (int64, error) {
+	queueKey := QueueToKey(tenantId, queue)
+	_, queueCloser, err := q.db.Get(queueKey)
+	if err != nil {
+		return 0, err
+	}
+	queueCloser.Close()
+
 	messageSnow := q.snow.Generate()
+
+	now := time.Now().UTC().Unix()
+	deliverAt := now + int64(delay)
 
 	// m := Message{id: &messageSnow, message: &message}
 	// q.ch <- m
 
 	batch := q.db.NewBatch()
-	id := messageSnow.IntBytes()
-	err := batch.Set(id[:], []byte(message), pebble.Sync)
+	defer batch.Close()
+
+	messageKey := MessageToKey(tenantId, queue, messageSnow.Int64())
+	err = batch.Set(messageKey, []byte(message), pebble.Sync)
 	if err != nil {
 		return 0, err
+	}
+
+	metadataKey := QueuedMetadataToKey(tenantId, queue, messageSnow.Int64(), deliverAt)
+	err = batch.Set(metadataKey, nil, pebble.Sync)
+	if err != nil {
+		return 0, err
+	}
+
+	for k, v := range kv {
+		kvKey := KVToKey(tenantId, queue, messageSnow.Int64(), k)
+		err = batch.Set(kvKey, []byte(v), pebble.Sync)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 
 	err = batch.Commit(pebble.Sync)
 	if err != nil {
 		return 0, err
-	}
-	err = batch.Close()
-	if err != nil {
-		// return 0, err
-		log.Println(messageSnow.String(), err)
 	}
 
 	return messageSnow.Int64(), nil
@@ -258,81 +377,12 @@ func (q *PebbleQueue) Dequeue(tenantId int64, queue string, numToDequeue int) ([
 
 	}
 
-	// return nil, nil
 	return rc, nil
 
-	// // batch := q.db.NewBatch()
-	// batch := q.db.NewIndexedBatch()
-	// i, err := batch.NewIter(&pebble.IterOptions{
-	// 	// i, err := q.db.NewIter(&pebble.IterOptions{
-	// 	UpperBound: []byte{1 << 7, 0, 0, 0, 0, 0, 0, 0},
-	// 	// LowerBound: []byte{1 << 7, 0, 0, 0, 0, 0, 0, 0},
-	// 	// LowerBound: []byte{'q', '2', 0, 0, 0, 0, 0, 0, 0x1},
-	// })
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// // val, closer, _ := batch.Get([]byte{25, 15, 114, 24, 30, 64, 16, 16})
-	// // id := binary.BigEndian.Uint64([]byte{25, 15, 114, 24, 30, 64, 16, 16})
-	// // v := make([]byte, len(val))
-	// // copy(v, val)
-	// // msg := &models.Message{
-	// // 	ID:      int64(id),
-	// // 	Message: v,
-	// // }
-	// // rc = append(rc, msg)
-	// // closer.Close()
-
-	// msgs := 0
-	// // log.Println(i)
-	// // log.Println(i.First())
-	// for kv := i.First(); kv; i.Next() {
-	// 	// log.Println(batch.Get([]byte{'x'}))
-	// 	// batch.Set([]byte{'x'}, []byte{1}, pebble.Sync)
-
-	// 	// log.Println(i.Key())
-	// 	id := binary.BigEndian.Uint64(i.Key())
-
-	// 	// log.Println(int64(id))
-	// 	// log.Println(id, msgs)
-
-	// 	v := make([]byte, len(i.Value()))
-	// 	copy(v, i.Value())
-
-	// 	msg := &models.Message{
-	// 		ID:      int64(id),
-	// 		Message: v,
-	// 	}
-	// 	rc = append(rc, msg)
-
-	// 	k := make([]byte, len(i.Key()))
-	// 	copy(k, i.Key())
-	// 	k[0] = k[0] | (1 << 7)
-	// 	batch.Set(k, v, pebble.Sync)
-
-	// 	batch.Delete(i.Key(), pebble.Sync)
-
-	// 	msgs++
-
-	// 	if msgs >= numToDequeue {
-	// 		break
-	// 	}
-	// }
-
-	// i.Close()
-
-	// err = batch.Commit(pebble.Sync)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// batch.Close()
-
-	// return rc, nil
 }
 
 func (q *PebbleQueue) Peek(tenantId int64, queue string, messageId int64) *models.Message {
+	// key: msgs (1) + tenantId (8) + queueName (80) + status (1) + deliverat (4) + msgid (8) = message
 	return nil
 }
 
