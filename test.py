c45c20962be57a0379d52c2ed6923e33ae3af895
zackees <z@zackees.com>
2024-07-02 22:09:40 -0700

    Add .venv to .gitignore and create test.py for SQS operations

---
+++ test.py
@@ -0,0 +1,40 @@
+import boto3
+import time
+
+# Configure the SQS client
+sqs = boto3.client("sqs", 
+                   region_name="us-east-1",
+                   aws_access_key_id="YOUR_ACCESS_KEY_ID",
+                   aws_secret_access_key="YOUR_SECRET_ACCESS_KEY",
+                   endpoint_url="http://localhost:3001")
+
+# Create the queue
+queue_name = "my-test-que-for-testing"
+response = sqs.create_queue(QueueName=queue_name)
+queue_url = response['QueueUrl']
+print(f"Created queue: {queue_url}")
+
+try:
+    # Perform operations
+    sqs.send_message(QueueUrl=queue_url, MessageBody="hello world")
+    print("Sent a message to the queue")
+
+    # Receive and print the message
+    response = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1)
+    if 'Messages' in response:
+        message = response['Messages'][0]
+        print(f"Received message: {message['Body']}")
+        
+        # Delete the message
+        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle'])
+        print("Deleted the message")
+    else:
+        print("No messages in the queue")
+
+    # Wait a moment to ensure all operations are completed
+    time.sleep(2)
+
+finally:
+    # Destroy the queue
+    sqs.delete_queue(QueueUrl=queue_url)
+    print(f"Destroyed queue: {queue_url}")
