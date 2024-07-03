3df939d4b3640540469430a8297644511708ffb0
zackees <z@zackees.com>
2024-07-02 18:50:56 -0700

    Delay nginx startup by 10 seconds

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 # Create nginx user and group
 adduser --system --no-create-home --shell /bin/false --group --disabled-login nginx
 
-# start nginx in the background
-#echo "running nginx"
-#nginx &
+# Start nginx in the background after a 10-second delay
+(sleep 10 && echo "running nginx" && nginx) &
 
 echo "running sqs emulator"
 run-app
