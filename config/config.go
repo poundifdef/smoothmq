4b68c311b082a1c0947f44dfc566c8b03d974bb9
Jay <jgoel@jays-air.mynetworksettings.com>
2024-07-09 11:08:23 -0400

    use kong for all configs

--- config/config.go
+++ config/config.go
@@ -1,60 +1,81 @@
 package config
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/alecthomas/kong"
+	kongyaml "github.com/alecthomas/kong-yaml"
 )
 
 type CLI struct {
-	Server struct{} `cmd:"server" help:"Run queue server"`
-	Tester struct {
-		SqsEndpoint string `help:"SQS endpoint" name:"endpoint" default:"http://localhost:3001" env:"SQS_ENDPOINT"`
-		Senders     int    `help:"" default:"0"`
-		Receivers   int    `help:"" default:"0"`
-		Messages    int    `help:"" default:"0"`
-	} `cmd:"tester" help:"Run tester"`
+	Server ServerCommand `cmd:"server" help:"Run queue server"`
+	Tester TesterCommand `cmd:"tester" help:"Run queue test tool"`
 
-	ConfigFile string `help:"Configuration file" name:"config" type:"path"`
+	Config kong.ConfigFlag `name:"config" help:"Configuration file"`
+	Log    LogConfig       `embed:"" prefix:"log-" name:"log"`
 }
 
-type Config struct {
-	Log    LogConfig    `koanf:"log"`
-	Server ServerConfig `koanf:"server"`
+type TesterCommand struct {
+	SqsEndpoint string `help:"SQS endpoint" name:"endpoint" default:"http://localhost:3001"`
+	Senders     int    `help:"" default:"0"`
+	Receivers   int    `help:"" default:"0"`
+	Messages    int    `help:"" default:"0"`
+}
+
+type ServerCommand struct {
+	SQS       SQSConfig       `embed:"" prefix:"sqs-" envprefix:"Q_SQS_"`
+	Dashboard DashboardConfig `embed:"" prefix:"dashboard-" envprefix:"Q_DASHBOARD_"`
+	SQLite    SQLiteConfig    `embed:"" prefix:"sqlite-" envprefix:"Q_SQLITE_"`
 }
 
 type LogConfig struct {
-	Pretty bool   `koanf:"pretty"`
-	Level  string `koanf:"level"`
-}
-
-type ServerConfig struct {
-	SQS       SQSConfig       `koanf:"sqs"`
-	Dashboard DashboardConfig `koanf:"dashboard"`
-	SQLite    SQLiteConfig    `koanf:"sqlite"`
+	Pretty bool   `name:"pretty"`
+	Level  string `name:"level"`
 }
 
 type SQLiteConfig struct {
-	Path string `koanf:"path"`
+	Path string `name:"path" help:"Path of SQLite file" default:"smoothmq.sqlite" env:"PATH"`
 }
 
 type SQSConfig struct {
-	Enabled bool     `koanf:"enabled"`
-	Port    int      `koanf:"port"`
-	Keys    []AWSKey `koanf:"keys"`
+	Enabled bool     `name:"enabled" default:"true" help:"Enable SQS protocol for queue" env:"ENABLED"`
+	Port    int      `name:"port" default:"3001" help:"HTTP port for SQS protocol" env:"PORT"`
+	Keys    []AWSKey `name:"keys" default:"DEV_ACCESS_KEY_ID:DEV_SECRET_ACCESS_KEY" env:"KEYS"`
 }
 
 type AWSKey struct {
-	AccessKey string `koanf:"access_key"`
-	SecretKey string `koanf:"secret_key"`
+	AccessKey string `name:"accesskey"`
+	SecretKey string `name:"secretkey"`
+}
+
+func (k *AWSKey) Decode(ctx *kong.DecodeContext) error {
+	var val string
+
+	err := ctx.Scan.PopValueInto("string", &val)
+	if err != nil {
+		return err
+	}
+
+	tokens := strings.Split(val, ":")
+	if len(tokens) != 2 {
+		return errors.New("AWS SQS key should be of the form access_key:secret_key")
+	}
+
+	k.AccessKey = tokens[0]
+	k.SecretKey = tokens[1]
+
+	return nil
 }
 
 type DashboardConfig struct {
-	Enabled bool `koanf:"enabled"`
-	Port    int  `koanf:"port"`
+	Enabled bool `name:"enabled" help:"Enable web dashboard" default:"true" env:"ENABLED"`
+	Port    int  `name:"port" help:"HTTP port for dashboard" default:"3000" env:"PORT"`
 }
 
 func Load() (string, *CLI, error) {
 	cli := &CLI{}
-	c := kong.Parse(cli)
+	c := kong.Parse(cli, kong.Configuration(kongyaml.Loader))
 
 	return c.Command(), cli, c.Error
 }
--- main.go
+++ main.go
@@ -9,19 +9,9 @@ import (
 	"q/models"
 	"q/queue/sqlite"
 	"q/tenants/defaultmanager"
-
-	_ "embed"
-
-	"github.com/knadh/koanf/parsers/yaml"
-	"github.com/knadh/koanf/providers/file"
-	"github.com/knadh/koanf/providers/rawbytes"
-	"github.com/knadh/koanf/v2"
 )
 
-//go:embed config.yaml
-var configData []byte
-
-func Run(command string, cli *config.CLI, cfg *config.Config, tenantManager models.TenantManager, queue models.Queue) {
+func Run(command string, cfg *config.CLI, tenantManager models.TenantManager, queue models.Queue) {
 	if tenantManager == nil {
 		tenantManager = defaultmanager.NewDefaultTenantManager(cfg.Server.SQS.Keys)
 	}
@@ -34,7 +24,7 @@ func Run(command string, cli *config.CLI, cfg *config.Config, tenantManager mode
 
 	switch command {
 	case "tester":
-		tester.Run(cli.Tester.Senders, cli.Tester.Receivers, cli.Tester.Messages, cli.Tester.SqsEndpoint)
+		tester.Run(cfg.Tester.Senders, cfg.Tester.Receivers, cfg.Tester.Messages, cfg.Tester.SqsEndpoint)
 	default:
 		smoothmq.Run(tenantManager, queue, cfg.Server)
 	}
@@ -48,26 +38,5 @@ func main() {
 		panic(err)
 	}
 
-	k := koanf.New(".")
-	cfg := &config.Config{}
-	parser := yaml.Parser()
-	var provider koanf.Provider
-
-	if cli.ConfigFile != "" {
-		provider = file.Provider(cli.ConfigFile)
-	} else {
-		provider = rawbytes.Provider(configData)
-	}
-
-	err = k.Load(provider, parser)
-	if err != nil {
-		panic(err)
-	}
-
-	err = k.Unmarshal("", cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	Run(command, cli, cfg, nil, nil)
+	Run(command, cli, nil, nil)
 }
